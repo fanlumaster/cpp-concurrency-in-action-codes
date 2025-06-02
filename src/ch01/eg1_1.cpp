c76045d299d296473c7085fd5e36023688040f0b
fanlumaster <1730976608@qq.com>
2025-06-03 07:59:56 +0800

    code format style

--- src/ch01/eg1_1.cpp
+++ src/ch01/eg1_1.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <thread>
 
-void hello() { std::cout << "Hello Concurrent World\n"; }
+void hello()
+{
+    std::cout << "Hello Concurrent World\n";
+}
 
-int main() {
-  std::thread t(hello);
-  t.join();
+int main()
+{
+    std::thread t(hello);
+    t.join();
 }
